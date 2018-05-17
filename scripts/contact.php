61294f8af11695563728f520e04e2d7ceccf5596
Kirilenko Danila <33377623+gc-group@users.noreply.github.com>
2018-05-17 21:19:46 +0300

    Add files via upload

---
+++ scripts/contact.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactmessage'])) {
+	$to = 'danila.kirilenko@hotmail.com'; // Your e-mail address here.
+	$body = "\nName: {$_POST['contactname']}\nEmail: {$_POST['contactemail']}\n\n\n{$_POST['contactmessage']}\n\n";
+	mail($to, "Message from TGOC. Bug or msg", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
+    }
+}
+?>
